aee0c5dc71aab3bf7e52fcdac6a9ef5b79e28dad
Victoria Louzada Poltronieri <65969480+victoriapoltronieri@users.noreply.github.com>
2022-08-18 08:21:14 -0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -63,7 +63,7 @@ def dia_zero(dias):
 
 
 def _main_():
-    if((datetime.now().time().hour==11)and(datetime.now().time().minute==18)):
+    if((datetime.now().time().hour==11)and(datetime.now().time().minute==23)):
        # api.update_status("Entrou aqui povo")
         dias = calcula_dias_restantes()
         frase_extra = random_tweet()
