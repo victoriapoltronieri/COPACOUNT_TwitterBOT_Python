c06a2484e416590d0d53b6b6934261c6134c87b7
Victoria Louzada Poltronieri <65969480+victoriapoltronieri@users.noreply.github.com>
2022-08-18 10:00:34 -0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -38,7 +38,22 @@ def random_tweet():
             "Ainda dá tempo de marcar o churrasco na casa da sogra pra assistir a seleção.",
             "#Hexa2022",
             "A skin de Maria Chuteira já tá atualizada.",
-            "Vamo assistir Casimiro reagindo aos jogos da copa juntos?"]
+            "Vamo assistir Casimiro reagindo aos jogos da copa juntos?",
+            "Sou Neymarzete desde criancinha.",
+            "*Vendo que os jogos da seleção são em dia de semana*\n\n*Eu pro meu patrão*: Não posso ir trabalhar pois gripá-lo-ei",
+            "Dia dos jogos do Brasil:\n- 24/11\n- 28/11\n- 02/12",
+            "waka waka ê ê"
+            "alexa toca waka_waka-shakira-2010.mp3",
+            "Bola na trave não altera o placar, bola na área sem ninguém pra cabecear",
+            "Ainda dá tempo de juntar os vizinhos e pintar a rua",
+            "Tô sonhando com ela          a Copa do Mundo",
+            "Eu: acho que já vou dormir boa noite bjs\nEu também: *buzzfeed ponto com* descubra qual cabelo do neymar você é",
+            "Já combinou o bolão com os amigos?",
+            "Minha linguagem de amor é presentes, pra me consquistar é só comprar o album da Copa com vários pacotes de figurinha",
+            "Agora a inflação foi longe demais... 4 conto o pacote de figurinha é um absurdo. Quem vai aparecer no protesto?",
+            "Thomas Muller mandou dizer oi",
+            "Última Copa do CR7 e do Messi. Nada mais justo do que a final ser Brasil e Tunísia pra eles aproveitarem bastante a cerimônia de encerramento."]
+    
     random_tweet = random.choice(tweets)
     return random_tweet
 
@@ -49,7 +64,7 @@ def calcula_dias_restantes():
     return (start-current).days
 
 def mais_de_um_dia(dias):
-    t = "Faltam " + str(dias) + " dias para a Copa do Mundo FIFA 2022 no Qatar!\n\n"
+    t = "Faltam " + str(dias) + " dias para a Copa do Mundo FIFA 2022 no Qatar.\n\n"
     return t
 
 def um_dia(dias):
@@ -57,7 +72,7 @@ def um_dia(dias):
     return t
 
 def dia_zero(dias):
-    t = "A Copa do Mundo FIFA 2022 começa HOJE!"
+    t = "Alô, amigos da Rede Globo! Copa do Mundo FIFA 2022 começa HOJE"
     return t
 
 
@@ -75,7 +90,7 @@ def _main_():
             tweet = um_dia(dias) + frase_extra
             api.update_status(tweet)
         
-        elif(dia_zero(dias)):
+        elif(dias == 0):
             tweet = dia_zero(dias)
             api.update_status(tweet)
         
@@ -84,11 +99,11 @@ def _main_():
 
 if __name__ == "__main__":
     while True:
-        if((datetime.now().time().hour==11) or (datetime.now().time().hour==8)):
+        if((datetime.now().time().hour==10) or (datetime.now().time().hour==11)):
             _main_()
             time.sleep(60)
 
-        elif((datetime.now().time().hour==9)):
+        elif((datetime.now().time().hour==12)):
             time.sleep(79200)
         else:
             time.sleep(3600)
